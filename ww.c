2e9f71b05d48d3b2a2954c6cadaa1463bdfa80f4
Rohit Rao <rohorao@gmail.com>
2021-02-28 03:26:26 -0500

    incorrect changes

--- ww.c
+++ ww.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "ww.h"
 
 #ifndef DEBUG
@@ -14,18 +15,33 @@ int main(int argc, char* argv[]){
     // If the first argument entered is invalid or not positive
     if(atoi(argv[1]) <= 0) return EXIT_FAILURE;
 
-    int wrapLength = atoi(argv[1]);
+    unsigned int wrapLen = atoi(argv[1]);
 
     /*          1 Input Reading             */
     // Variables needed for reading input
-    char buffer[wrapLength];
-    int wordCount = 0;
-    int charLength = 0;
+    char buffer[wrapLen];
+    unsigned int wordCount = 0, charLen = 0;
+    unsigned long int bigWordSize = wrapLen, bigWordIndex = 0; // the biggest word can theoretically be unsigned int large and we need an additional space for '\0'
+    char* regWord = calloc(wrapLen+1, sizeof(char)), bigWord = calloc(bigWordSize+1, sizeof(char)); // init to all null terminators
     int bytes = 0; // used for the while loop to detect end of file
 
-
     while ((bytes = read(0, buffer, 1)) > 0) {
-        charLength++;
+        charLen++;
+        if(charLen >= wrapLen){
+            if(isEmpty(regWord)){
+                if(DEBUG) printf("Copying regword into bigword here\n");
+                strcpy(bigWord, regWord);
+            }
+            if(charLen >= 2 * wrapLen){
+                if(DEBUG) printf("Current word is bigger than 2 * the maximum\n");
+                wordCount = 0;
+            }
+            if(isspace(bytes)!=0){
+                if(DEBUG) printf("Current char is not a space\n");
+                addElementToString(bigWord, bytes, &bigWordSize, &bigWordIndex);
+                continue;
+            }
+        }
         write(0, buffer, bytes);
     }
     if (bytes < 0) {
--- ww.h
+++ ww.h
@@ -1,2 +1,14 @@
 // Storing functions here so that the .c file is less cluttered
 
+// 0 is false, 1 is true
+
+
+void setEmpty(char* string){
+    string[0] = '\0';
+}
+
+int isEmpty(char* string){
+    if (string == 0 || string[0] == '\0') return 1;
+    return 0;
+}
+
