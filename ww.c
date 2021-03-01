aba5b5a0695614dd47dd5554fa0d2c87f3912311
Rohit Rao <rohorao@gmail.com>
2021-03-01 03:11:47 -0500

    added comments

--- ww.c
+++ ww.c
@@ -10,16 +10,15 @@
 #endif
 
 int main(int argc, char* argv[]){
-    /*          Checking Input Validity         */
 
     // Possible Errors That Could Exist
     // Something might be wrong with wordIndex -- it might be off by 1 or 2
+    // There may be extra spaces or newline chars at the end of lines -- I can't tell if they exist in vscode console
 
 
-    // No arguments entered or too many arguments entered
-    if(argc < 2 || argc > 2) return EXIT_FAILURE;
-    // If the first argument entered is invalid or not positive
-    if(atoi(argv[1]) <= 0) return EXIT_FAILURE;
+    /*          Checking Input Validity         */
+    if(argc < 2 || argc > 2) return EXIT_FAILURE;           // No arguments entered or too many arguments entered
+    if(atoi(argv[1]) <= 0) return EXIT_FAILURE;             // If the first argument entered is invalid or not positive
 
     unsigned int wrapLen = atoi(argv[1]);
 
@@ -27,71 +26,69 @@ int main(int argc, char* argv[]){
     /*          1 Input Entered             */
 
     char buffer[1];                                         // buffer to save each char
-    unsigned int wordCount = 0, charLen = 0, wordIndex = 0; // variables to keep track of specific counters
+    unsigned int wordCount = 0, lineLen = 0, wordIndex = 0; // variables to keep track of specific counters
     char* word = calloc(wrapLen+1, sizeof(char));           // init to all null terminators
     int isBigWord = 0;                                      // used to detect if the current word is a big word (0-false, 1-true)
     int byte = 0;                                           // used for the while loop to detect end of file
 
     while ((byte = read(0, buffer, 1)) > 0) {
-        if(buffer[0] == 10){                                // carriage return
-            printWord(word, wordIndex);
-            printChar('\n');
-            free(word);
-            return EXIT_SUCCESS;
+        if(buffer[0] == 10){                                // carriage return case
+            printWord(word, wordIndex);                     // Write the word to the current line
+            printChar('\n');                                // Write a newline
+            free(word);                                     // Free the string word to avoid memory leaks
+            return EXIT_SUCCESS;                            // Exit Successfully
         }
-        if(charLen < wrapLen){
-            if(!isspace(buffer[0])){
-                word[wordIndex] = buffer[0];
-                wordIndex = wordIndex + 1;
-                charLen = charLen + 1;
-            }else{
-                // CARRIAGE RETURN CASE WAS HERE
-                printWord(word, wordIndex);
-                if(isBigWord == 0){
-                    printChar(' ');
-                    wordCount = wordCount + 1;
-                    charLen = charLen +1;
-                }else{
-                    printChar('\n');
-                    isBigWord = 0;
-                    charLen = 0;
+        if(lineLen < wrapLen){                              // If the length of the line is less than the maximum allowed wrap length
+            if(!isspace(buffer[0])){                            // If the current char is not a space
+                word[wordIndex] = buffer[0];                        // Set the last unused index of the string word to the current char
+                wordIndex = wordIndex + 1;                          // Update/increment the last unsed index of the string word
+                lineLen = lineLen + 1;                              // Update/increment the length of the line
+            }else{                                              // If the current char is a space
+                printWord(word, wordIndex);                         // Write the word to the current line
+                if(isBigWord == 0){                                 // If the word is not bigger than the allowed wrap length
+                    printChar(' ');                                     // Write a space
+                    wordCount = wordCount + 1;                          // Update/Increment the number of words on the line
+                    lineLen = lineLen + 1;                              // Update/Increment the length of the line
+                }else{                                              // If the word is bigger than the allowed wrap length
+                    printChar('\n');                                    // Write a newline character (go to the next line)
+                    isBigWord = 0;                                      // Set the isBigWord flag to false
+                    lineLen = 0;                                        // Set the length of the line to 0
                 }
-                clearWord(word, wordIndex);
-                wordIndex = 0;
+                clearWord(word, wordIndex);                         // Clear the string word
+                wordIndex = 0;                                      // Reset the last unsed index of the string word back to 0
             }
-        }else if(charLen == wrapLen){
-            if(isspace(buffer[0])){
-                // CARRIAGE RETURN CASE WAS HERE
-                if(wordCount >= 0){
-                    printWord(word, wordIndex);
-                    printChar('\n');
-                    isBigWord = 0;
-                    charLen = 0;
-                    wordCount = 0;
-                    clearWord(word, wordIndex);
-                    wordIndex = 0;
-                }else return throwError(word);
-            }else{
-                if(wordCount > 0){
-                    if(isBigWord == 0){
-                        printChar('\n');
+        }else if(lineLen == wrapLen){                       // If the length of the line is equal to the maximum allowed wrap length
+            if(isspace(buffer[0])){                             // If the current char is a space
+                if(wordCount >= 0){                                 // If the number of words on the line is >= 0 //NOT SURE IF THIS LINE IS NEEDED
+                    printWord(word, wordIndex);                         // Write the word to the current line
+                    printChar('\n');                                    // Write a newline character (go to the next line)
+                    isBigWord = 0;                                      // Set the isBigWord flag to false
+                    lineLen = 0;                                        // Set the length of the line to 0
+                    wordCount = 0;                                      // Set the number of words on the line to 0
+                    clearWord(word, wordIndex);                         // Clear the string word
+                    wordIndex = 0;                                      // Reset the last unsed index of the string word back to 0
+                }else return throwError(word);                      // Throw an error //NOT SURE IF THIS LINE IS NEEDED
+            }else{                                              // If the current char is not a space
+                if(wordCount > 0){                                  // If there are multiple words on the line
+                    if(isBigWord == 0){                                 // If it is not a word that is bigger than the wrap length
+                        printChar('\n');                                    // Write a newline character (go to the next line)
                     }
-                    charLen = wordIndex+1;
-                }else if (wordCount == 0){
-                    charLen = 0;
-                    isBigWord = 1;
-                }else return throwError(word);
-                word[wordIndex] = buffer[0];
-                printWord(word, wordIndex);
-                wordCount = 0;
-                clearWord(word, wordIndex);
-                wordIndex = 0;
+                    lineLen = wordIndex+1;                                  // Set the line length to be the length of used string word
+                }else if (wordCount == 0){                          // If there are no words on the line yet (this is one word bigger than the wrap length)
+                    lineLen = 0;                                        // Set the length of the line to 0
+                    isBigWord = 1;                                      // Set the big word flag to true (1)
+                }else return throwError(word);                      // Throw an error //NOT SURE IF THIS LINE IS NEEDED
+                word[wordIndex] = buffer[0];                        // Set the last unused index of the string word to the current char
+                printWord(word, wordIndex);                         // Write the word to the current line
+                wordCount = 0;                                      // Set the number of words on the line to 0
+                clearWord(word, wordIndex);                         // Clear the string word
+                wordIndex = 0;                                      // Reset the last unsed index of the string word back to 0
             }
-        } else return throwError(word);
+        } else return throwError(word);                         // Throw an error //NOT SURE IF THIS LINE IS NEEDED
     }
-    if (byte < 0) {
+    if (byte < 0) {                                         // Original error from Prof code thrown in the case of an error in the while loop
         perror("Read error");
     }
-    free(word);
-    return EXIT_SUCCESS;
+    free(word);                                             // Free the string word
+    return EXIT_SUCCESS;                                    // Exit Successfully
 }
--- ww.h
+++ ww.h
@@ -1,40 +1,31 @@
 // Storing functions here so that the .c file is less cluttered
 
-// 0 is false, 1 is true
-
-
-void setEmpty(char* string){
-    string[0] = '\0';
-}
-
-int isEmpty(char* string){
-    if (string == 0 || string[0] == '\0') return 1;
-    return 0;
-}
-
+// Write the word to stdout -- Might need to change this function later to use file descriptor instead
 void printWord(char* string, unsigned int wordIndex){
-    // for(int i = 0; i < wordIndex; i++){
     write(0, string, wordIndex+1); //not sure if the +1 is needed
-    // }
 }
 
+// Write a char to stdout -- Mainly using this to write newlines and spaces
 void printChar(char value){
     char string[1] = {value};
     write(0, string, 1);
 }
 
+// Clears the string word by setting all elements to 0 -- This may get inefficient if the wrap length is extremely large
 void clearWord(char* string, unsigned int wordIndex){
     for(int i = 0; i <= wordIndex; i++){
         string[i] = '\0';
     }
 }
 
+// Handles the error cases
 int throwError(char* word){
     perror("This is not supposed to happen.");
     free(word);
     return EXIT_FAILURE;
 }
 
+// Used this print for debugging purposes
 void debugPrintWord(char* string, unsigned int wordIndex){
     for(int i = 0; i < wordIndex; i++){
         printf("%c", string[i]);
